837a3702a65b7207a52a5db3dfb3145433db7216
NJ-001 <neeljeure24@gmail.com>
2026-04-23 19:37:31 +0530

    Delete Backend directory

--- Backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .complaint import Complaint
-from .comment import Comment
-from .vote import Vote
-from .notification import Notification
-from .internal_note import InternalNote
